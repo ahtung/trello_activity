d22f0fdf9d29122792c875afbf8f07ae2609fc62
Onur  Kucukkece <onurkucukkece@Onurs-MacBook-Pro.local>
2014-01-04 00:17:25 +0200

    modified js functions

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ $(document).ready(function(e) {
 		var card_id = '7BxmX3GG';
 	
 		Trello.get("cards/"+ card_id +"/actions/",{filter:'updateCard'}, function(actions){
-			
+			var difference
 			$.each(actions,function(index, action){	
 				var start_time = atChangedLists(action,"To Do", "Doing");
 				var end_time = atChangedLists(action,"Doing", "Done");
-				var difference = timeDifference(start_time, end_time);
-				$("#output").append("Card " + action.date + " completed in " + difference + "seconds.<br>");
+				difference = timeDifference(start_time, end_time);
 			});
+			$("#output").append("Card  completed in " + difference + " seconds.<br>");
 		});
 	};
 	
@@ -28,7 +28,7 @@ $(document).ready(function(e) {
 	var timeDifference = function(fromTime, toTime){
 		fromTimeDate = new Date(fromTime);
 		toTimeDate = new Date(toTime);
-		diff=Math.round(fromTimeDate - toTimeDate/1000/60/60/24)
+		diff=Math.round(fromTimeDate - toTimeDate/1000)
 		console.log('dif : '+diff);
 	}
 	
