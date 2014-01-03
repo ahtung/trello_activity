3d9a10a488e8fe87914eb607763971db8c7c9235
Onur  Kucukkece <onurkucukkece@Onurs-MacBook-Pro.local>
2014-01-03 22:03:32 +0200

    Script moved into document ready

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
-var onAuthorize = function() {
-    updateLoggedIn();
-    $("#output").empty();
-    
-	Trello.cards.get("9AvXrTfo", function(card){
-        
-    	console.log(card);
-        
-    });
-
-};
-
-var updateLoggedIn = function() {
-    var isLoggedIn = Trello.authorized();
-    $("#loggedout").toggle(!isLoggedIn);
-    $("#loggedin").toggle(isLoggedIn);        
-};
-    
-var logout = function() {
-    Trello.deauthorize();
-    updateLoggedIn();
-};
-                          
-Trello.authorize({
-    interactive:false,
-    success: onAuthorize
+$(document).ready(function(e) {
+    var onAuthorize = function() {
+		updateLoggedIn();
+		$("#output").empty();
+		
+		Trello.cards.get("9AvXrTfo", function(card){
+			
+			console.log(card);
+			$("#output").text('card id:'+card.id);
+		});
+	
+	};
+	
+	var updateLoggedIn = function() {
+		var isLoggedIn = Trello.authorized();
+		$("#loggedout").toggle(!isLoggedIn);
+		$("#loggedin").toggle(isLoggedIn);        
+	};
+		
+	var logout = function() {
+		Trello.deauthorize();
+		updateLoggedIn();
+	};
+							  
+	Trello.authorize({
+		interactive:false,
+		success: onAuthorize
+	});
+	
+	$("#connectLink").click(function(){
+		Trello.authorize({
+			type: "popup",
+			success: onAuthorize
+		})
+	});
+		
+	$("#disconnect").click(logout);
 });
-
-$("#connectLink")
-.click(function(){
-    Trello.authorize({
-        type: "popup",
-        success: onAuthorize
-    })
-});
-    
-$("#disconnect").click(logout);
