6abdc83a4d099e93191ae3c4e6a8c914ea3cbcad
Onur  Kucukkece <onurkucukkece@Onurs-MacBook-Pro.local>
2014-01-03 21:22:28 +0200

    Created new js file ,

--- index.php
+++ index.php
@@ -7,62 +7,13 @@
 <title>Digital Services – Philips</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="https://api.trello.com/1/client.js?key=fe590d6d0ce9a6826d52b0207afc1a98"></script>
+<script type="text/javascript" src="js/script.js"></script>
 <script type="text/javascript">
    
-var onAuthorize = function() {
-    updateLoggedIn();
-    $("#output").empty();
-    
-    Trello.members.get("me", function(member){
-        $("#fullName").text(member.fullName);
-    
-        var $cards = $("<div>")
-            .text("Loading Cards...")
-            .appendTo("#output");
 
-        // Output a list of all of the cards that the member 
-        // is assigned to
-        Trello.get("members/me/cards", function(cards) {
-            $cards.empty();
-            $.each(cards, function(ix, card) {
-                $("<a>")
-                .attr({href: card.url, target: "trello"})
-                .addClass("card")
-                .text(card.name)
-                .appendTo($cards);
-            });  
-        });
-    });
-
-};
-
-var updateLoggedIn = function() {
-    var isLoggedIn = Trello.authorized();
-    $("#loggedout").toggle(!isLoggedIn);
-    $("#loggedin").toggle(isLoggedIn);        
-};
-    
-var logout = function() {
-    Trello.deauthorize();
-    updateLoggedIn();
-};
-                          
-Trello.authorize({
-    interactive:false,
-    success: onAuthorize
-});
-
-$("#connectLink")
-.click(function(){
-    Trello.authorize({
-        type: "popup",
-        success: onAuthorize
-    })
-});
-    
-$("#disconnect").click(logout);
 
 </script>
+
 </head>
 <body>
 <div id="wrap">
@@ -77,6 +28,9 @@ $("#disconnect").click(logout);
     </div>
     
     <div id="output"></div>
+    
+    http GET "https://api.trello.com/1/boards/4d5ea62fd76aa1136000000c/actions" "since==Apr 1 2013 EDT" "before==May 1 2013 EDT" "limit==1000" "filter==commentCard"
+
 </div>   
 </div>
 </body>
